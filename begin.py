a7a837593044c86b5562b75bcc1dc9c26a1a6498
Vipin Kumar <31645405+vipinkjonwal@users.noreply.github.com>
2017-10-25 23:07:06 +0530

    Update begin.py

--- begin.py
+++ begin.py
@@ -1,5 +1,8 @@
 from tkinter import *
 root = Tk()
+#Line 2 will create a box which is first thing we will be needing.
 theLabel = Label(root,text="This is Tkinter")
 theLabel.pack()
+#pack() - line 5 -  method is used to put widgets inside the window.
 root.mainloop()
+#mainloop() - line 7 - is used to display window on the screen.
